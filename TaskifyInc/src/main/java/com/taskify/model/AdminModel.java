57997fa86144734c63b772db7e425abb63748b1a
Susam Pudasaini <pudasainisusam@gmail.com>
2025-05-18 15:37:05 +0545

    Add Admin adds to users_admin table

--- TaskifyInc/src/main/java/com/taskify/model/AdminModel.java
+++ TaskifyInc/src/main/java/com/taskify/model/AdminModel.java
@@ -2,22 +2,24 @@ package com.taskify.model;
 
 public class AdminModel {
     private int adminId;
+    private int userId;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
 
-    // Constructor
-    public AdminModel(String firstName, String lastName, String email, String password) {
+    public AdminModel(int userId, String firstName, String lastName, String email, String password) {
+        this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
     }
 
-    // Getters and setters
+    public int getUserId() { return userId; }
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
     public String getEmail() { return email; }
     public String getPassword() { return password; }
 }
+
